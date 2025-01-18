4d8d65375a19ac4bf8a35608f0c28d7751430d25
Darshan Patel <darshhpatel37@gmail.com>
2025-01-18 17:10:12 +0530

    pipeline is complete

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,8 +26,8 @@ from sklearn.ensemble import (
 import mlflow
 # from urllib.parse import urlparse
 
-# import dagshub
-# #dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+import dagshub
+dagshub.init(repo_owner='darshhpatel37', repo_name='NetworkSecurity', mlflow=True)
 
 # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
 # os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
@@ -140,8 +140,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
-        #model pusher
-        # save_object("final_model/model.pkl",best_model)
+        # model pusher
+        save_object("final_model/model.pkl",best_model)
         
 
         ## Model Trainer Artifact
